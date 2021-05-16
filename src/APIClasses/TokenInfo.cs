45e332ce4d9b5dd597110e6b2a8286e069996c8b
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-05-16 02:13:17 +0200

    Added API key permission check
    Added Exception and reason handling

---
+++ src/APIClasses/TokenInfo.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Hardstuck.GuildWars2.Builds.APIClasses
+{
+    internal class TokenInfo
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("permissions")]
+        public List<string> Permissions { get; set; }
+    }
+}
